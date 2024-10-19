7ba8ffa413acc9fd1c8d70d37e4adfa9c1f94c04
Junxun Yin <markjx.yin@gmail.com>
2024-10-19 18:57:07 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
+# 激活 Python 虚拟环境
+. /opt/venv/bin/activate
+
+# 初始化参数变量
 PARAMS=
 
+# 如果 EFB_PROFILE 变量非空，则添加到参数中
 if [ -n "$EFB_PROFILE" ]; then
   PARAMS="$PARAMS -p $EFB_PROFILE"
 fi
 
+# 添加额外的 EFB 参数
 PARAMS="$PARAMS $EFB_PARAMS"
 
+# 启动 ehforwarderbot 并传入构建好的参数
 eval "ehforwarderbot $PARAMS"
